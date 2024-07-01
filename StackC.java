e8a688fd62c7c8acc2863511c88da96b61d3b7eb
Rajeev Sahu <rajeevsahu210@gmail.com>
2024-07-01 06:23:38 +0530

    added stack

---
+++ StackC.java
@@ -0,0 +1,41 @@
+import java.util.*;;
+
+public class StackC {
+    public static void pushAtBottom(Stack<Integer> s, int data) {
+        if (s.isEmpty()) {
+            s.push(data);
+            return;
+        }
+        int top = s.pop();
+        pushAtBottom(s, data);
+        s.push(top);
+    }
+
+    public static String reverseString(String str) {
+        Stack<Character> s = new Stack<>();
+        int idx = 0;
+        while (idx < str.length()) {
+            s.push(str.charAt(idx));
+            idx++;
+        }
+        StringBuilder result = new StringBuilder("");
+        while (!s.isEmpty()) {
+            char curr = s.pop();
+            result.append(curr);
+        }
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        Stack<Integer> s = new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        pushAtBottom(s, 4);
+        while (!s.isEmpty()) {
+            System.out.println(s.pop());
+        }
+        String str = "abc";
+        System.out.println(reverseString(str));
+    }
+}
