91ea833d1fe5cd542a36a62fa8879f16b2eb829e
Rajeev Sahu <rajeevsahu210@gmail.com>
2024-07-07 18:24:00 +0530

    updated Stack

--- StackC.java
+++ StackC.java
@@ -26,16 +26,56 @@ public class StackC {
         return result.toString();
     }
 
+    public static void reverseStack(Stack<Integer> s) {
+        if (s.isEmpty()) {
+            return;
+        }
+        int top = s.pop();
+        reverseStack(s);
+        pushAtBottom(s, top);
+    }
+
+    public static void printStack(Stack<Integer> s) {
+        while (!s.isEmpty()) {
+            System.out.println(s.pop());
+        }
+    }
+
+    public static void stockSpan(int[] stocks, int[] span) {
+        Stack<Integer> s = new Stack<>();
+        span[0] = 1;
+        s.push(0);
+        for (int i = 1; i < stocks.length; i++) {
+            int currPrice = stocks[i];
+            while (!s.isEmpty() && currPrice > stocks[s.peek()]) {
+                s.pop();
+            }
+            if (s.isEmpty()) {
+                span[i] = i + 1;
+            } else {
+                int prevHigh = s.peek();
+                span[i] = i - prevHigh;
+            }
+            s.push(i);
+        }
+    }
+
     public static void main(String[] args) {
         Stack<Integer> s = new Stack<>();
         s.push(1);
         s.push(2);
         s.push(3);
-        pushAtBottom(s, 4);
-        while (!s.isEmpty()) {
-            System.out.println(s.pop());
-        }
+        // pushAtBottom(s, 4);
+        // printStack(s);
         String str = "abc";
-        System.out.println(reverseString(str));
+        // System.out.println(reverseString(str));
+        reverseStack(s);
+        // printStack(s);
+        int[] stocks = { 100, 80, 60, 70, 60, 85, 100 };
+        int[] span = new int[stocks.length];
+        stockSpan(stocks, span);
+        for (int i : span) {
+            System.out.println(i);
+        }
     }
 }
